da1e3e44d1f959e3ebdbe85773581e94de1db269
EvanPavley <epavley@sva.edu>
2019-06-14 14:27:24 -0400

    added proper validations

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,14 +2,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//custom validation for email
+var validateEmail = function(email) {
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email)
+};
+
 var UserSchema = new Schema({
   name: {
     type: String,
-    required: 'Please enter your name'
+    required: 'Please enter your name',
+    minlength: 1,
+    maxlength: 50,
   },
   email: {
     type: String,
-    required: 'Please enter your email'
+    required: 'Please enter your email',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    minlength: 1,
+    maxlength: 50,
   },
   updates: {
     type: Boolean,
@@ -17,16 +28,25 @@ var UserSchema = new Schema({
   },
   location: {
     type: String,
-    required: 'Please enter your location'
+    default: "New York, NY"
   },
   bio: {
     type: String,
-    required: 'Please enter a bio'
+    required: 'Please enter a bio',
+    minlength: 1,
+    maxlength: 250,
   },
   password: {
     type: String,
-    required: 'Please enter your password'
+    required: 'Please enter your password',
+    minlength: 1,
+    maxlength: 50,
   },
+});
+
+UserSchema.pre('findOneAndUpdate', function(next) {
+  this.options.runValidators = true;
+  next();
 });
 
 module.exports = mongoose.model('Users', UserSchema);
